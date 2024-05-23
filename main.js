b99c52985f5f1eee00a44bd36b34a60d2b66516c
Saidmashhud Habibzoda <habibzoda.s@karimov.group>
2024-05-23 22:25:54 +0500

    fix canvas width, height

--- main.js
+++ main.js
@@ -1,45 +1,11 @@
+import "./style.css";
 
+import { Application } from "@splinetool/runtime";
 
-import './style.css'
+const canvas = document.getElementById("canvas3d");
 
-// Import spline scene via vanillajs runtime:
-
-import { Application } from '@splinetool/runtime';
-
-// make sure you have a canvas in the body
-const canvas = document.getElementById('canvas3d');
-
-// start the application and load the scene (selfhosted)
 const spline = new Application(canvas);
-spline.load('/bunny_cube_scene.splinecode');
+spline.load("/bunny_cube_scene.splinecode");
 
 // TODO: does not work?
-spline.setBackgroundColor('red');
-
-// import spline viewer webcomponent
-import { SplineViewer } from '@splinetool/viewer';
-
-// Vite scaffold demo code:
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app .vite-demo-content').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
+spline.setBackgroundColor("red");
