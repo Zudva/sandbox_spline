454397d84cc70913d35dfc2204ca58d969e6ea5f
Slava Zudvin <zudva@protonmail.com>
2024-05-24 21:44:16 +0300

    Update scene

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { Application } from "@splinetool/runtime";
 const canvas = document.getElementById("canvas3d");
 
 const spline = new Application(canvas);
-spline.load("/interface_ii.spline");
+spline.load("/hover_scroll_effect.spline");
 
 // TODO: does not work?
 spline.setBackgroundColor("red");
