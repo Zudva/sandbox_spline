e7bab32d82bf5cffc341ea0f9f90a6ec3deda139
Slava Zudvin <zudva@protonmail.com>
2024-05-31 02:44:08 +0300

    Update interstelar

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { Application } from "@splinetool/runtime";
 const canvas = document.getElementById("canvas3d");
 
 const spline = new Application(canvas);
-spline.load("/interstelar.spline");
+spline.load("/interstelar_v2.spline");
 
 // TODO: does not work?
 spline.setBackgroundColor("red");
